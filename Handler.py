476624d82a67b869c3b997f6d442e032c7b27836
colakogluezgi00 <ezgi.colakoglu@hotmail.com>
2024-05-08 15:30:57 +0300

    handler

--- Handler.py
+++ Handler.py
@@ -1,3 +1,8 @@
+from rdflib import Graph, RDF, URIRef, Literal
+import pandas as pd
+from sqlite3 import connect
+from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
+
 class Handler (object):
     def __init__(self, dbPathOrUrl):
         self.dbPathOrUrl = dbPathOrUrl
@@ -16,14 +21,122 @@ class UploadHandler(Handler):
         pass
 
 class MetadataUploadHandler(UploadHandler):
-    pass
+    def pushDataToDb(self, path,dbPathOrUrl):
+        super().pushDataToDb(path)
+        super().dbPathOrUrl
+        my_graph = Graph()
+        NauticalChart = URIRef("https://github.com/DataScienceProject23-24/DataProject/tree/main/resources/NauticalChart") 
+        ManuscriptPlate = URIRef("https://github.com/DataScienceProject23-24/DataProject/tree/main/resources/ManuscriptPlate") 
+        ManuscriptVolume = URIRef("https://schema.org/Manuscript")
+        PrintedVolume = URIRef("https://schema.org/Book")
+        PrintedMaterial = URIRef("https://github.com/DataScienceProject23-24/DataProject/tree/main/resources/PrintedMaterial")
+        Herbarium = URIRef("https://github.com/DataScienceProject23-24/DataProject/tree/main/resources/Herbarium")
+        Specimen = URIRef("https://github.com/DataScienceProject23-24/DataProject/tree/main/resources/Specimen")
+        Painting = URIRef("https://schema.org/Painting")
+        Model = URIRef("https://github.com/DataScienceProject23-24/DataProject/tree/main/resources/Model")
+        Map = URIRef("https://schema.org/Map")
+
+        #attributes
+        id = URIRef("https://schema.org/identifier")
+        title = URIRef("https://schema.org/title")
+        date = URIRef("https://schema.org/dateCreated")
+        owner = URIRef("https://schema.org/acquiredFrom")
+        place = URIRef("https://schema.org/location")
+        name = URIRef("https://schema.org/name")
+
+        #relations 
+        hasAuthor = URIRef("https://schema.org/author")
+
+
+
+        #POPULATING THE RDF GRAPH 
+
+
+        #I create a base url to be shared among all my objects
+        base_url = "https://github.com/DataScienceProject23-24/DataProject/tree/main/resources/"
+
+
+        #Now i read my DataFrame
+        CulturalHeritageObject = pd.read_csv(path, keep_default_na=False, 
+                        dtype={
+                            "Id" : "string",
+                            "Title" : "string",
+                            "Date" : "string",
+                            "Author" : "string",
+                            "Owner" : "string",
+                            "Place" : "string",
+                            "Type" : "string"
+                        })
+
+
+        #now I insert all the objects in the graph 
+        for idx, row in CulturalHeritageObject.iterrows():
+            local_id = "object-" + str(idx) #i create an id for every object 
+
+            subj = URIRef(base_url + local_id)
+
+
+            #I add all the statements:
+            if row["Type"] == "Nautical chart":
+                my_graph.add((subj, RDF.type, NauticalChart))
+
+            elif row["Type"] == "Manuscript plate":
+                my_graph.add((subj, RDF.type, ManuscriptPlate))
+            
+            elif row["Type"] == "Manuscript volume":
+                my_graph.add((subj, RDF.type, ManuscriptVolume))
+            
+            elif row["Type"] == "Printed volume":
+                my_graph.add((subj, RDF.type, PrintedVolume))
+            
+            elif row["Type"] == "Printed material":
+                my_graph.add((subj, RDF.type, PrintedMaterial))
+            
+            elif row["Type"] == "Herbarium":
+                my_graph.add((subj, RDF.type, Herbarium))
+            
+            elif row["Type"] == "Specimen":
+                my_graph.add((subj, RDF.type, Specimen))
+            
+            elif row["Type"] == "Painting":
+                my_graph.add((subj, RDF.type, Painting))
+            
+            elif row["Type"] == "Model":
+                my_graph.add((subj, RDF.type, Model))
+            
+            elif row["Type"] == "Map":
+                my_graph.add((subj, RDF.type, Map))
+            
+            if row["Id"]:
+                my_graph.add((subj, id, Literal(row["Id"])))
+            if row["Title"]:
+                my_graph.add((subj, title, Literal(row["Title"])))
+            if row["Date"]:
+                my_graph.add((subj, date, Literal(row["Date"])))
+            if row["Owner"]:
+                my_graph.add((subj, owner, Literal(row["Owner"])))
+            if row["Place"]:
+                my_graph.add((subj, place, Literal(row["Place"])))
+
+        #upload the graph on triplestore
+
+        store = SPARQLUpdateStore()
+        endpoint = dbPathOrUrl #my proxy for putting the data in the database
+
+        #now I open the connection 
+        store.open((endpoint, endpoint))
+
+        for triple in my_graph.triples((None, None, None)): #specyifing None I ask for all the triples 
+            store.add(triple)
+
+        store.close()
+
+
 
 class ProcessDataUploadHandler(UploadHandler):
     def pushDataToDb(self, path, dbPathOrUrl):
         super().pushDataToDb(path)
         super().dbPathOrUrl
-        import pandas as pd
-        from sqlite3 import connect
         process_json = pd.read_json(path)
         
         object_ids=process_json[["object id"]]
@@ -46,4 +159,11 @@ class ProcessDataUploadHandler(UploadHandler):
             norm_df[2].to_sql("Modelling", con, if_exists="replace", index=False)
             norm_df[3].to_sql("Optimizing", con, if_exists="replace", index=False)
             norm_df[4].to_sql("Exporting", con, if_exists="replace", index=False)
+
+
+
+
+
+
+
 
