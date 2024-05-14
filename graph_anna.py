b99fc10541a5ab2b057c64a0578beef56db62d49
Anna Pasetto <anna.pasetto2001@gmail.com>
2024-05-14 11:29:44 +0200

    final graph

--- graph_anna.py
+++ graph_anna.py
@@ -1,4 +1,6 @@
 from rdflib import Graph, RDF, URIRef, Literal
+import pandas as pd
+from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 
 my_graph = Graph()
 
@@ -27,65 +29,62 @@ name = URIRef("https://schema.org/name")
 hasAuthor = URIRef("https://schema.org/author")
 
 
-
-#POPULATING THE RDF GRAPH 
-from pandas import read_csv, Series
-
 #I create a base url to be shared among all my objects
 base_url = "https://github.com/DataScienceProject23-24/DataProject/tree/main/resources/"
 
 
 #Now i read my DataFrame
-CulturalHeritageObject = read_csv("DataProject/resources/meta.csv", keep_default_na=False, 
-                  dtype={
-                      "Id" : "string",
-                      "Title" : "string",
-                      "Date" : "string",
-                      "Author" : "string",
-                      "Owner" : "string",
-                      "Place" : "string",
-                      "Type" : "string"
-                })
+CulturalHeritageObject = pd.read_csv("meta.csv",
+                            keep_default_na=False,
+                            dtype={
+                               "Id": "string",
+                               "Type": "string",
+                               "Title": "string",
+                               "Date":"string",
+                               "Author": "string",
+                               "Owner": "string",
+                               "Place": "string",
+                            })
 
 
-#now I insert all the objects in the graph 
 for idx, row in CulturalHeritageObject.iterrows():
-    local_id = "object-" + str(idx) #i create an id for every object 
+    local_object_id = "object-" + str(idx) #i create an id for every object 
 
-    subj = URIRef(base_url + local_id)
+    subj = URIRef(base_url + local_object_id)
 
+#now I populate the graph 
 
-    #I add all the statements:
+#I add all the statements:
     if row["Type"] == "Nautical chart":
         my_graph.add((subj, RDF.type, NauticalChart))
 
     elif row["Type"] == "Manuscript plate":
         my_graph.add((subj, RDF.type, ManuscriptPlate))
-    
+
     elif row["Type"] == "Manuscript volume":
         my_graph.add((subj, RDF.type, ManuscriptVolume))
-    
+
     elif row["Type"] == "Printed volume":
         my_graph.add((subj, RDF.type, PrintedVolume))
-    
+
     elif row["Type"] == "Printed material":
         my_graph.add((subj, RDF.type, PrintedMaterial))
-    
+
     elif row["Type"] == "Herbarium":
         my_graph.add((subj, RDF.type, Herbarium))
-    
+
     elif row["Type"] == "Specimen":
         my_graph.add((subj, RDF.type, Specimen))
-    
+
     elif row["Type"] == "Painting":
         my_graph.add((subj, RDF.type, Painting))
-    
+
     elif row["Type"] == "Model":
         my_graph.add((subj, RDF.type, Model))
-    
+
     elif row["Type"] == "Map":
         my_graph.add((subj, RDF.type, Map))
-    
+
     if row["Id"]:
         my_graph.add((subj, id, Literal(row["Id"])))
     if row["Title"]:
@@ -97,46 +96,46 @@ for idx, row in CulturalHeritageObject.iterrows():
     if row["Place"]:
         my_graph.add((subj, place, Literal(row["Place"])))
 
+for idx, authors_list in enumerate(CulturalHeritageObject["Author"]):
+    if authors_list: 
+        authors = authors_list.split(";") #managin multiple authors (checked)
+        for author in authors:
+            name, author_id = author.split(" (")            
+            author_id = author_id[:-1] #i remove )
 
-print(len(my_graph)) # --> 209 perchè senza author 
+            local_object_id = "object-" + str(idx)
+            subj = URIRef(base_url + local_object_id)
+            my_graph.add((subj, hasAuthor, Literal(name)))
 
 
+print(len(my_graph)) #--> 237 (correct)
 
-#upload the graph on triplestore
-from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
+
 
 store = SPARQLUpdateStore()
-endpoint = 'http://127.0.0.1:9999/blazegraph/sparql' #my proxy for putting the data in the database
 
-#now I open the connection 
+endpoint = "http://127.0.0.1:9999/blazegraph/sparql"
 store.open((endpoint, endpoint))
 
-for triple in my_graph.triples((None, None, None)): #specyifing None I ask for all the triples 
+for triple in my_graph.triples((None, None, None)):
     store.add(triple)
 
 store.close()
 
 
-
-
-#cd "C:\Users\annap\Desktop\blazergraph"
+#cd C:\Users\annap\Desktop\blazegraph
 #java -server -Xmx1g -jar blazegraph.jar
-#http://127.0.0.1:9999/blazegraph/
 
 
-#PREFIXES
 #PREFIX res:    <https://github.com/DataScienceProject23-24/DataProject/tree/main/resources/>
 #PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 #PREFIX schema: <https://schema.org/>
 
-#PROVA DOMANDA 
+
 #SELECT*
-#WHERE{
+#WHERE {
 #  ?s rdf:type schema:Book.
 #  ?s ?p ?o.
 #}
-    
-#CI SONO I DATI SU AUTHOR PERCHè è ALL'INTERNO DEL CSV ORIGINALE. 
-# DEVE ESSERE MODIFICATO IN RELATION
 
-#PROVA 
+
